972109c602a4e9ebc4c81a1da1bee4e609d5464d
Nithish kumar k <nithish81812@gmail.com>
2025-08-18 22:54:28 +0530

    Printing 10 even numbers using java

---
+++ EvenNumbers.java
@@ -0,0 +1,8 @@
+class EvenNumbers {
+  public static void main(String args[]) {
+    System.out.println("Even Numbers are:");
+    for(int i = 2; i <= 20; i += 2) {
+       System.out.println(i);
+    }
+  }
+}
