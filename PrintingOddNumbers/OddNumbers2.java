516441dc7c5c14dc481ea175d1b7367c1d5d0fe0
Nithish kumar k <nithish81812@gmail.com>
2025-08-18 23:08:28 +0530

    printing 10 odd numbers using java program part-2

---
+++ PrintingOddNumbers/OddNumbers2.java
@@ -0,0 +1,10 @@
+class OddNumbers2 {
+  public static void main(String args[]) {
+    System.out.println("Odd Numbers Are :");
+    for(int i = 1; i <= 20 ; i++) {
+      if(i%2 != 0) {
+        System.out.println(i);
+      }
+    }
+  }
+}
