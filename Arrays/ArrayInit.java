c22c141b3d71294c7ae8d3dccc845e4079a81caf
Nithish kumar k <nithish81812@gmail.com>
2025-08-20 23:47:45 +0530

    pushing code

---
+++ Arrays/ArrayInit.java
@@ -0,0 +1,8 @@
+class ArrayInit {
+  public static void main(String args[]) {
+    int[] arr = {1,2,3,4,5};
+    for(int i = 1; i < arr.length; i++) {
+       System.out.println(arr[i]);
+    }
+  }
+}
