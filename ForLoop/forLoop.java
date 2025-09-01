0df56a64861609a0519ee77755293b1d9cf113f5
Nithish kumar k <nithish81812@gmail.com>
2025-09-01 20:33:34 +0530

    pushing for loop code to print positive numbers

---
+++ ForLoop/forLoop.java
@@ -0,0 +1,13 @@
+class forLoop {
+    public static void main(String args[]) {
+        int n = 5;
+        //for loop to print 5 numbers from 0 to 5
+        for(int i = 0; i <= n; i++) {
+           System.out.println(i);
+        }
+        //for loop to print 5 numbers from 5 to 0
+        for(int i = n; i >= 0 ; i--) {
+           System.out.println(i);
+        }
+    }
+}
