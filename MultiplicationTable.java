1143215b0fcde48faa91b2a756e031aa3d03b07b
K-Nithish-Kumar <nithish81812@gmail.com>
2025-10-16 00:04:02 +0530

    Adding Multiplication table java program

---
+++ MultiplicationTable.java
@@ -0,0 +1,12 @@
+public class MultiplicationTable {
+    public static void printTable(int n) {
+        for(int i = 1; i <=10; i++){
+           System.out.println(n + " * " + i + " = " + n*i);
+        }
+    }
+
+ public static void main(String nithish[]) {
+     int n = 5;
+     printTable(5);
+ }
+} 
