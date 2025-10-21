26d0a4d27a195f2772bd9d15d5edbad449354a05
K-Nithish-Kumar <nithish81812@gmail.com>
2025-10-21 16:55:03 +0530

    Adding java program for swapping two numbers without using the third variable

---
+++ SwapTwoNumbers/SwapNumbers02.java
@@ -0,0 +1,13 @@
+//Swapping two numbers without using third variable
+
+public class SwapNumbers02 {
+    public static void main(String args[]) {
+       int a = 2;
+       int b = 3;
+       System.out.println("Before Swapping :" + "a" + "=" + a + " b" + "=" + b);
+       a = a + b;
+       b = a - b;
+       a = a - b;
+       System.out.println("After Swapping:" + "a" + "=" + a + " b" + "=" + b);
+    }
+}
