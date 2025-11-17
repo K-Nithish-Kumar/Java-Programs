dbdfaa459e17cc9fd19760a551a81bcf2f95bfa8
K-Nithish-Kumar <nithish81812@gmail.com>
2025-11-17 23:18:35 +0530

    Adding division of two numbers

---
+++ AES/Division.java
@@ -0,0 +1,14 @@
+public class Division {
+       public static int division(int a, int b) {
+            int result = a / b;
+            return result;
+       }
+       
+   public static void main(String args[]) {
+       int a = 20;
+       int b = 11;
+       System.out.println("The division of two number is: " + division(a,b));
+   }
+}
+       
+  
