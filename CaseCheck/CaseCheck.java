fe161ad35ca2e5469445ce19148080d941a63a10
K-Nithish-Kumar <nithish81812@gmail.com>
2025-12-06 17:01:11 +0530

    Adding java program for checking the case for the given character

---
+++ CaseCheck/CaseCheck.java
@@ -0,0 +1,16 @@
+//Case checking for entered character
+
+import java.util.Scanner;
+
+public class CaseCheck {
+   public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        char ch = in.next().trim().charAt(0);
+        
+	if(ch >= 'a' && ch <= 'z') {
+            System.out.println("The given character " +ch + " by user is Lowercase");
+        } else {
+	    System.out.println("The given character " +ch + " by user is Uppercase");
+	}
+   }
+}
